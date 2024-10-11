45930b32396d6de896b7779a12db0fb5f1baf11e
francisfra <kiilukitonyi@gmail.com>
2024-10-11 10:23:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add any interactive elements or functionality here.
+// For example, a simple alert to greet your visitors:
+window.onload = function() {
+    alert("Welcome to Francis Kiilu Kitonyi's Portfolio!");
+}
